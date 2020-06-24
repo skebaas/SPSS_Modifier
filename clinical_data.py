55d2f7313801f78250f7c362517e644389d9a208
skebaas <54587831+skebaas@users.noreply.github.com>
2020-06-23 23:03:46 -0400

    Generalized script to include more visits

--- clinical_data.py
+++ clinical_data.py
@@ -33,7 +33,6 @@ class Visit():
         for row in df.iterrows():
             row_names_raw = row[1]['Raw data name ']
             row_names_master = row[1]['Masterfile name ']
-            #print(col_dict[row[1]])
             #check to see if the raw_data_name column equals " no raw data"
             if row_names_raw.lower() == "no raw data".lower():
                 self.special_cases.append(row[1]['Masterfile name '])
@@ -42,22 +41,88 @@ class Visit():
 
     def write_New_Data(self):
 
-        temp_dict = {}
+        output_dict = {}
         input_csv = pd.read_csv(self.raw_data, delimiter = ',')
+        total_rows = len(input_csv)
+        special_cases = self.special_cases
+        
 
+        # Run method that pulls the data from the input csv
         self.get_Visit_Data()
-        for key, value in self.visit_dict.items():
+
+        #uncomment to see what Values are being included in the special_cases list
+        #print(special_cases)
+
+
+        # List that holds all VOCAB_Q* (Used for calculating Sum for VOCAB_raw_score)
+        VOCAB_QUESTIONS = [key for key, val in self.visit_dict.items() if "VOCAB_Q" in key]
+        MATRIX_ITEMS = [key for key, val in self.visit_dict.items() if "MATRIX_ITEM_" in key]
+
+        
+        ###Now the complicated part###
+        '''
+        The following section is looking through the new and old names:
+
+        new_value: (String) What the raw data variable is being renamed to
+        old_value: (String) Variable representing the value from the raw data 
+        '''
+        for new_value, old_value in self.visit_dict.items():
             try:
-                if value.lower() == "no raw data".lower():
-                    temp_dict[key] = "NaN"
+
+                if new_value in special_cases or new_value in VOCAB_QUESTIONS or new_value in MATRIX_ITEMS:
+                    print(f"RUNNING: Performing neccessary calculations for {new_value}")
+                    output_dict[new_value] = {}
+                    for index, row in input_csv.iterrows():
+
+                        if new_value in ["VOCAB_Q1_corrected", "VOCAB_Q2_corrected", "VOCAB_Q3_corrected", "VOCAB_Q4_corrected"]:
+                            output_dict[new_value][index] = 1
+                        elif new_value in ["VOCAB_Q5_corrected", "VOCAB_Q6_corrected", "VOCAB_Q7_corrected", "VOCAB_Q8_corrected"]:
+                            output_dict[new_value][index] = 2
+                        elif new_value in VOCAB_QUESTIONS:
+                            try:
+                                vocab_value = int(row[old_value]) -1
+                            except ValueError:
+                                vocab_value = row[old_value]
+                            output_dict[new_value][index] = vocab_value
+                        elif new_value == "VOCAB_raw_score":
+                            subj_total = 0
+                            for question in VOCAB_QUESTIONS:
+                                #Check to see if the Value is an integer.  If it is not, force val_holder to be 0
+                                try:
+                                    #Variable to hold value for calculation of VOCAB_raw_score
+                                    var_holder = int(output_dict[question][index])
+                                    subj_total += var_holder
+                                except ValueError:
+                                    var_holder = 0
+                                    subj_total += var_holder
+                            output_dict[new_value][index] = subj_total
+
+                        # Doing similar method for Matrix Items
+                        elif new_value in MATRIX_ITEMS:
+                            try:
+                                matrix_value = int(row[old_value]) -1
+                            except ValueError:
+                                matrix_value = row[old_value]
+                            output_dict[new_value][index] = vocab_value
+                        elif new_value == "MATRIX_raw_score":
+                            subj_total = 0 
+                            for item in MATRIX_ITEMS:
+                                try:
+                                    var_holder = int(output_dict[item][index])
+                                    subj_total += var_holder
+                                except ValueError:
+                                    pass
+                            output_dict[new_value][index] = subj_total
+
 
                 else:
-                    temp_dict[key] = input_csv[value]
+                    output_dict[new_value] = input_csv[old_value]
 
             except KeyError:
-                    print(f"WARNING: Could not find data for {value}")
+                output_dict[new_value] = "ERROR"
+                print(f"WARNING: Could not find {old_value} matching data for {new_value}")
 
-        df = pd.DataFrame.from_dict(temp_dict)
+        df = pd.DataFrame.from_dict(output_dict)
         df.to_csv(self.output_name)
 
     def check_Path(self):
